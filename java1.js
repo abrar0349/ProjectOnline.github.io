6e3b12b8d981788fb52d334c4027f61039362800
idreestanveer <idreesahmed1612@gmail.com>
2023-04-04 23:52:46 +0500

    My first online project folder on github

---
+++ java1.js
@@ -0,0 +1,10 @@
+document.write("java tuturial");
+
+
+var prp1 = prompt("What Should price have of 1 kg chicken")
+var gm = 1000 / prp1;
+
+
+var prp2 = prompt("How much rupees do you want to buy?")
+var b = gm * prp2;
+document.write("<br><br>"+"Then you will get ",b," Gram")
